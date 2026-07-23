aa3caf8941a9115e58d444473584b1edfe5e5e7a
trungkiendev203 <trungkiendev203@gmail.com>
2026-07-23 11:13:34 +0700

    Override SQLite path dynamically to bypass Vercel dashboard environment variables overrides

--- bookstore/config/database.php
+++ bookstore/config/database.php
@@ -35,7 +35,9 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => file_exists('/var/task/user/public/database.sqlite')
+                ? '/var/task/user/public/database.sqlite'
+                : env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
